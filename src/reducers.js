0a794f116b956523707d47ae96d38b2ed33d1a3d
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-11-19 21:48:44 +0700

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,40 +1,130 @@
-import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS } from "./actions";
 
 function reducer(state, action){
-    switch(action.type){
-        case CLEAR_CART:
-            return { ...state, cart:[] };
-        case DECREASE:
-            console.log("DECREASE: ", "you decreased amount.");
-        case INCREASE:
-            // console.log("INCREASE: ", "you increased amount.");
-            let tempCart = state.cart.map((cartItem) => {
+    // switch(action.type){
+    //     case CLEAR_CART:
+    //         return { ...state, cart:[] };
+    //     case DECREASE:
+    //         // console.log("DECREASE: ", "you decreased amount.");
+    //         let tempCartDecrease = [];
+    //         if(action.payload.amount === 1){
+    //             console.log('INCREASE: ', 'it is one')
+    //         } else {
+    //             tempCartDecrease = state.cart.map((cartItem) => {
+    //                 // console.log(cartItem);
+    //                 if(cartItem.id === action.payload.id){
+    //                     cartItem = {
+    //                         ...cartItem, 
+    //                         amount: cartItem.amount - 1
+    //                     }
+    //                 }
+    //                 return cartItem;
+    //             });
+    //         }
+    //         return {
+    //             ...state,
+    //             cart: tempCartDecrease
+    //         };
+    //     case INCREASE:
+    //         // console.log("INCREASE: ", "you increased amount.");
+    //         let tempCartIncrease = state.cart.map((cartItem) => {
+    //             // console.log(cartItem);
+    //             if(cartItem.id === action.payload.id){
+    //                 cartItem = {
+    //                     ...cartItem, 
+    //                     amount: cartItem.amount + 1
+    //                 }
+    //             }
+    //             return cartItem;
+    //         });
+    //         return {
+    //             ...state, // copy old state value
+    //             cart:tempCartIncrease
+    //         }
+    //     case REMOVE:
+    //         // console.log("REMOVE: ", "you removed "+action.payload.id);
+    //         return {
+    //             ...state, 
+    //             cart:state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
+    //         };
+    //     default:
+    //         return state;
+    // }
+    
+    if(action.type === CLEAR_CART){
+        return { ...state, cart:[] };
+    }
+            
+    if(action.type === DECREASE){
+        // console.log("DECREASE: ", "you decreased amount.");
+        let tempCart = [];
+        if(action.payload.amount === 1){
+            // console.log('INCREASE: ', 'it is one')
+            tempCart = state.cart.filter((cartItem)=>cartItem.id !== action.payload.id);
+        } else {
+            tempCart = state.cart.map((cartItem) => {
                 // console.log(cartItem);
                 if(cartItem.id === action.payload.id){
                     cartItem = {
                         ...cartItem, 
-                        amount: cartItem.amount + 1
+                        amount: cartItem.amount - 1
                     }
                 }
                 return cartItem;
             });
-            return {
-                ...state, // copy old state value
-                cart:tempCart
-            }
-        case REMOVE:
-            // console.log("REMOVE: ", "you removed "+action.payload.id);
-            return {
-                ...state, 
-                cart:state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
-            };
-        default:
-            return state;
+        }
+        return {
+            ...state,
+            cart: tempCart
+        };
     }
-    // if(action.type === CLEAR_CART){
-    //     return { ...state, cart:[]};
-    // }
-    // return state;
+    if(action.type === INCREASE){
+        // console.log("INCREASE: ", "you increased amount.");
+        let tempCart = state.cart.map((cartItem) => {
+            // console.log(cartItem);
+            if(cartItem.id === action.payload.id){
+                cartItem = {
+                    ...cartItem, 
+                    amount: cartItem.amount + 1
+                }
+            }
+            return cartItem;
+        });
+        return {
+            ...state, // copy old state value
+            cart:tempCart
+        }
+    }
+    if(action.type === REMOVE){
+        // console.log("REMOVE: ", "you removed "+action.payload.id);
+        return {
+            ...state, 
+            cart:state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
+        };
+    }
+    if(action.type === GET_TOTALS){
+        // console.log("TOTALS: ");
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+            const { price, amount } = cartItem;
+            const itemTotal = price * amount;
+
+            cartTotal.total += itemTotal;
+            cartTotal.amount += amount;
+
+            return cartTotal;
+        },{
+            total:0, 
+            amount: 0
+        });
+        total = parseFloat(total.toFixed(2));
+        return {
+            ...state,
+            total,
+            amount
+        };
+    }
+    return state;
+    
 }
 
 export default reducer;
