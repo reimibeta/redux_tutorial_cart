3d9dacee6b06208a74b437ce96429fc441acf546
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-19 12:28:18 +0700

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,26 @@
-import { DECREASE, INCREASE, CLEAR_CART } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from "./actions";
 
 function reducer(state, action){
-    if(action.type === CLEAR_CART){
-        return { ...state, cart:[]};
+    switch(action.type){
+        case CLEAR_CART:
+            return { ...state, cart:[] };
+        case DECREASE:
+            console.log("DECREASE: ", "you decreased amount.");
+        case INCREASE:
+            console.log("INCREASE: ", "you increased amount.");
+        case REMOVE:
+            // console.log("REMOVE: ", "you removed "+action.payload.id);
+            return {
+                ...state, 
+                cart:state.cart.filter((cartItem)=>cartItem.id !== action.payload.id)
+            };
+        default:
+            return state;
     }
-    return state;
+    // if(action.type === CLEAR_CART){
+    //     return { ...state, cart:[]};
+    // }
+    // return state;
 }
 
 export default reducer;
