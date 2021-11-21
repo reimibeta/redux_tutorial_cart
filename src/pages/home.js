905286d723e8ceca1b17b663a03170de350ce5e2
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-21 11:06:40 +0700

    .

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+
+    render(){
+        return (
+            <main>
+                <h1>Home</h1>
+            </main>
+        );
+    }
+}
+
+export default Home;
---
+++ src/store/actions/auth.actions.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+// export function login(data){
+//     return dispatch => {
+//         return axios.post('http://p.pcrpallet.com/api/users/user_token/', data).then(res => {
+//             const token = res.data.token;
+//             localStorage.setItem('jwtToken', token);
+//         });
+//     }
+// }
