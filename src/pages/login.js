7827ea6b6a97006adf1e1ddb441497efe245e040
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-21 12:27:03 +0700

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,31 +1,44 @@
 import React, { Component } from "react";
-import { Form, Button } from "react-bootstrap";
+// import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 class Login extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {value: ''};
+    
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //   }
+    
+    //   handleChange(event) {
+    //     this.setState({value: event.target.value});
+    //   }
+    
+      handleSubmit(event) {
+        // alert('A name was submitted: ' + this.state.value);
+        axios.post('http://p.pcrpallet.com/api/users/user_token/', {'username': 'reimi', 'password': 'iwtdstw'}).then(res => {
+            // const token = res.data.token;
+            // localStorage.setItem('jwtToken', token);
+            console.log("token: ", res);
+        });
+        event.preventDefault();
+      }
 
     render(){
         return (
             <main>
-                <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                </Form>
+                <form onSubmit={this.handleSubmit}>
+                {/* <label>
+                    Name:
+                    <input type="text" name="name" value="reimi" />
+                </label>
+                <label>
+                    Password:
+                    <input type="text" name="password" value="iwtdstw" />
+                </label> */}
+                <input type="submit" value="Submit" />
+                </form>
             </main>
         );
     }
